c7adff37f4a7dc0314d15f7f40f380297999f063
Jason Lee <jawc@hotmail.com>
2018-01-01 12:38:24 +0800

    update centos 7

--- centos/centos7.sh
+++ centos/centos7.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
+#update package
+yum install -y epel-release
+yum update -y
+yum install -y net-tools telnet nfs-utils lrzsz nano chrony yum-utils curl
+
+# sync time now
+timedatectl set-timezone Asia/Taipei
+systemctl enable chronyd
+systemctl start chronyd
+sudo chronyc -a makestep
+
+# install docker
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+
+# change docker storage driver to overlay2
+mkdir -p /etc/docker/
+>/etc/docker/daemon.json cat << EOF 
+{
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+# change max open file for docker
+mkdir -p /etc/systemd/system/docker.service.d/
+>/etc/systemd/system/docker.service.d/override.conf << EOF 
+[Service]
+LimitNOFILE=1000000  
+LimitMEMLOCK=infinity
+EOF
+systemctl daemon-reload  
+
+systemctl enable docker
+systemctl restart docker
+
+# install docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 # user level
 if grep -Fxq "DefaultLimitCORE=infinity" /etc/systemd/user.conf
 then
@@ -48,6 +91,7 @@ fi
 
 >/etc/sysctl.conf cat << EOF 
 vm.swappiness = 1
+vm.max_map_count=262144
 net.ipv4.ip_local_port_range = 1024 65535 
 net.ipv4.tcp_fin_timeout = 30
 net.ipv4.tcp_max_syn_backlog = 65536
@@ -64,6 +108,9 @@ net.core.rmem_default = 8388608
 net.core.wmem_default = 8388608
 net.core.rmem_max = 16777216
 net.core.wmem_max = 16777216
+net.bridge.bridge-nf-call-ip6tables = 1  
+net.bridge.bridge-nf-call-iptables = 1  
+net.bridge.bridge-nf-call-arptables = 1
 fs.aio-max-nr=1048576
 fs.file-max = 1048575
 kernel.panic = 1
@@ -73,17 +120,11 @@ EOF
 
 
 
-# install default package
-[ -z "$(rpm -qa|grep ntp)" ] && yum install ntp -y
 
-[ -z "$(rpm -qa|grep nano)" ] && yum install nano -y
 
-\cp -rf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime &>>install.log
 
-systemctl stop ntpd
 
-/usr/sbin/ntpdate asia.pool.ntp.org
 
-systemctl enable ntpd
 
-systemctl start ntpd
+
+
